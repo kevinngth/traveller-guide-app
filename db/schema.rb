a4aff159cbf3003362a076d8168465ece02e7318
kevinngth <kevin.ng.th@gmail.com>
2019-11-12 11:38:33 +0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2019_11_12_020229) do
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
+  create_table "guides", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "travellers", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
