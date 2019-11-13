dde36010e43e1cc9763cb85f9c68eb7cb241ce04
Nathanael Tan <nathanael.tan92@gmail.com>
2019-11-13 15:10:16 +0800

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_11_13_060326) do
 
-ActiveRecord::Schema.define(version: 2019_11_13_034529) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
