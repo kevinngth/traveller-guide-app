cc47b06735e15cd9aea8bf5f7070ddd92f823400
kevinngth <kevin.ng.th@gmail.com>
2019-11-13 16:12:24 +0800

    updated navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,25 @@
 Rails.application.routes.draw do
+
+  root 'travellers#index'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-   post 'travellers/' => 'travellers#createsearch'
-
+  post 'travellers/' => 'travellers#createsearch'
   get 'travellers/dashboard' => 'travellers#index'
   get 'travellers/findguide' => 'travellers#findguide'
-  root 'travellers#index'
   get '/travellers/becomeaguide' =>  'travellers#becomeaguide', as: 'user'
   post '/travellers/becomeaguide' => 'travellers#makeguide'
+
   resources :travellers
+
   resources :guides do
     resources :experiences
   end
+
   resources :reviews
 
-
-
-
   resources :conversations do
-
     resources :messages
-
   end
-  
-
-
-
 
 end
