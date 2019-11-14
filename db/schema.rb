23f467275ee9c13aa2c34ff6f8ec37cc898d7ab9
Nathanael Tan <nathanael.tan92@gmail.com>
2019-11-14 10:49:05 +0800

    users can now give ratings based on a range

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_060326) do
+ActiveRecord::Schema.define(version: 2019_11_14_012406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_060326) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
     t.index ["guide_id"], name: "index_reviews_on_guide_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
